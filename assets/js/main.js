f43ca4280714eba7eb2133209e9dc9f47b3941b1
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-02-19 15:51:03 +0100

    add if and esle if for price calc

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,9 @@ let userDistance = prompt("Scrivi qui i kilometri da percorere")
 console.log (Number(userAge), Number (userDistance));
 
 console.log(userDistance * 0.21);
+
+if (userAge < 18) {
+    console.log((userDistance * 0.21) / (100) * (80));
+} else if (userAge > 65){
+    console.log((userDistance * 0.21) / (100) * (60));
+}
